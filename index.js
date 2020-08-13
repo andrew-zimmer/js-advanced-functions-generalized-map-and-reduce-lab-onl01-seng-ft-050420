9b5a155cfecfb74e58763ba549af408276391535
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-13 16:16:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, func){
+    const newArray = []
+    for (let x of array){
+        newArray.push(func(x))
+    }
+    return newArray
+}
+
+function reduce(array, func, start = true){
+    let total = start
+    for (let x of array){
+        total = func(x, total)
+    }
+    if (start === true){
+        if (typeof total === 'number'){
+            return total -1
+        }
+        return total
+    }
+    return total
+}
